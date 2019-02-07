5705a48718fac3a59b876484025053c3cbf2dd19
rusolm <leandromarquesp88@gmail.com>
2019-02-07 10:07:36 +0100

    feat(redux-saga): connect saga to redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducer';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(rootReducer);
+import { watchGnomes } from './sagas';
+
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(watchGnomes);
 
 export default store;
