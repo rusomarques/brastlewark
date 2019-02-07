b63631d4cd4606b7a1d78c4beda24c0e709aec03
rusolm <leandromarquesp88@gmail.com>
2019-02-07 10:54:00 +0100

    feat(Gnomes.js): wrap gnome with link - route to gnome details

--- src/components/Gnomes/Gnomes.js
+++ src/components/Gnomes/Gnomes.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Gnome from '../Gnome/Gnome';
 
 const Gnomes = props => {
   return props.gnomes.map(gnome => {
-    return <Gnome key={gnome.id} gnome={gnome} />;
+    return (
+      <Link key={gnome.id} to={`/gnome/${gnome.id}`}>
+        <Gnome gnome={gnome} />
+      </Link>
+    );
   });
 };
 
