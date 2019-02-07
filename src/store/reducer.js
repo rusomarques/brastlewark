841b4e526a856c2b15c3a0618bab53c5db1aacfb
rusolm <leandromarquesp88@gmail.com>
2019-02-07 15:52:45 +0100

    feat(reducer): handle search action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,21 @@
 import * as actionTypes from './actionTypes';
 
 const initialState = {
-  gnomes: null
+  gnomes: null,
+  searchResults: null
+};
+
+const searchGnomes = (state, action) => {
+  const lowercaseSearchInput = action.searchInput.toLowerCase();
+
+  const filteredGnomes = state.gnomes.filter(gnome => {
+    return gnome.name.toLowerCase().includes(lowercaseSearchInput);
+  });
+
+  return {
+    ...state,
+    searchResults: filteredGnomes
+  };
 };
 
 const reducer = (state = initialState, action) => {
@@ -11,6 +25,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         gnomes: action.gnomes
       };
+    case actionTypes.SEARCH_GNOME:
+      return searchGnomes(state, action);
     default:
       return state;
   }
