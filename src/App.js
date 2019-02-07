9a9ea68623a44b0fc10f9935dd91ab8f3e24541c
rusolm <leandromarquesp88@gmail.com>
2019-02-07 10:11:42 +0100

    feat(App.js): renders GnomesBrowser.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import GnomesBrowser from './containers/GnomeBrowser';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <GnomesBrowser />
       </div>
     );
   }
