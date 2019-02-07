edd6ea8d7b6e121407c02ce99dd13e9b2f9a5e5d
rusolm <leandromarquesp88@gmail.com>
2019-02-07 10:57:44 +0100

    feat(App.js): add route to GnomeDetail

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import GnomeBrowser from './containers/GnomeBrowser/GnomeBrowser';
+import GnomeDetail from './containers/GnomeDetail/GnomeDetail';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
+          <Route path="/gnome/:id" component={GnomeDetail} />
           <Route path="/" component={GnomeBrowser} />
           <Redirect to="/" />
         </Switch>
