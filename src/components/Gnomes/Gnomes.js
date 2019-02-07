50146140dfc057a631cca6f325c5be7287f9d400
rusolm <leandromarquesp88@gmail.com>
2019-02-07 14:13:32 +0100

    style(Gnomes):

--- src/components/Gnomes/Gnomes.js
+++ src/components/Gnomes/Gnomes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Gnome from '../Gnome/Gnome';
+import classes from './Gnomes.module.css';
 
 const Gnomes = props => {
   return props.gnomes.map(gnome => {
     return (
-      <Link key={gnome.id} to={`/gnome/${gnome.id}`}>
-        <Gnome gnome={gnome} />
-      </Link>
+      <div className={classes.Gnome}>
+        <Link className={classes.Link} key={gnome.id} to={`/gnome/${gnome.id}`}>
+          <Gnome gnome={gnome} />
+        </Link>
+      </div>
     );
   });
 };
