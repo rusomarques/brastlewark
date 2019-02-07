10030567bc0af880d2b3111b95db7ab299eb5e58
rusolm <leandromarquesp88@gmail.com>
2019-02-07 16:59:00 +0100

    feat(Gnomes.js): render paginated list of gnomes

--- src/components/Gnomes/Gnomes.js
+++ src/components/Gnomes/Gnomes.js
@@ -5,9 +5,18 @@ import Gnome from '../Gnome/Gnome';
 import classes from './Gnomes.module.css';
 
 const Gnomes = props => {
-  return props.gnomes.map(gnome => {
+  const paginationSelector = 'pagin-selector';
+
+  let numOfGnomes;
+  if (props.pages) {
+    numOfGnomes = props.pages * props.perPage;
+  } else {
+    numOfGnomes = 1337;
+  }
+  const paginatedGnomes = props.gnomes.slice(0, numOfGnomes);
+  return paginatedGnomes.map(gnome => {
     return (
-      <div key={gnome.id} className={classes.Gnome}>
+      <div key={gnome.id} className={`${classes.Gnome} ${paginationSelector}`}>
         <Link className={classes.Link} to={`/gnome/${gnome.id}`}>
           <Gnome gnome={gnome} />
         </Link>
