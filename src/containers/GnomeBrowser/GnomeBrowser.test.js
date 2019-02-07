ecabea59447f3648a9b1bd41b2e4e4d9f3f44862
rusolm <leandromarquesp88@gmail.com>
2019-02-07 23:58:05 +0100

    test(GnomeBrowser): render Search component and Spinner or Gnomes depending if gnomes loaded

---
+++ src/containers/GnomeBrowser/GnomeBrowser.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { GnomeBrowser } from './GnomeBrowser';
+
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Gnomes from '../../components/Gnomes/Gnomes';
+import Search from '../Search/Search';
+import * as mocks from './../../store/tests/mocks';
+
+configure({ adapter: new Adapter() });
+
+describe('<GnomeBrowser />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<GnomeBrowser onInitGnomes={() => {}} />);
+  });
+
+  it('should render a <Search /> if />', () => {
+    expect(wrapper.find(Search)).toHaveLength(1);
+  });
+
+  it('should render a <Spinner /> if elements if gnomes not loaded', () => {
+    expect(wrapper.find(Spinner)).toHaveLength(1);
+  });
+
+  it('should render a <Gnomes /> if gnomes are loaded', () => {
+    wrapper.setProps({ gnomes: mocks.gnomes });
+    expect(wrapper.find(Gnomes)).toHaveLength(1);
+  });
+});
