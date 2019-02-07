e53ae24e18b070d822f30688c10bb75cc776c2b1
rusolm <leandromarquesp88@gmail.com>
2019-02-07 10:14:12 +0100

    feat(GnomesBrowser.js): connect to redux and in compDidMount dispatch initGnomes to fetch api

--- src/containers/GnomeBrowser.js
+++ src/containers/GnomeBrowser.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { initGnomes } from './../store/actions';
 
 class GnomesBrowser extends Component {
+  componentDidMount() {
+    this.props.onInitGnomes();
+  }
+
   render() {
     return <div>GnomesList</div>;
   }
 }
 
-export default GnomesBrowser;
+const mapStateToProps = state => {
+  return {
+    gnomes: state.gnomes
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onInitGnomes: () => dispatch(initGnomes())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GnomesBrowser);
